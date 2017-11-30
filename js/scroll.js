a55c649beb95b7e7ddb3ce64a42328f03d2d171c
jchurch2112 <32181630+jchurch2112@users.noreply.github.com>
2017-11-30 09:55:02 -0800

    upload pinball movement

--- js/scroll.js
+++ js/scroll.js
@@ -17,21 +17,42 @@ window.onscroll = function(){
   //if (y > 120) {ball.style.top = 12 + "px";}
   //if (y > 160) {ball. style.top = 12 + y * 1.0 + "px";}
   if (y < 320) {ball.style.left = y + "px";}
+  if (y > 40) {ball.style.top = "24px";}
+   if (y >100){ball.style.top = 12 + y * 1.0 + "px";}
+
   //ball bounce on "ANIMATION" --end
-  if(y > 1360) {ball.style.left = "1px";}
- if (y > 1550)  {ball.style.left = "320px";}
-  if (y>1733) {ball.style.left = "1px";}
-  if (y > 1866)  {ball.style.left = "320px";}
-  if(y > 2000) {ball.style.left = "1px";}
-  if (y > 2133)  {ball.style.left = "320px";}
+  if(y > 1360) {ball.style.left =  "1px";}
+
+ if (y > 1700)  {ball.style.left = "320px";}
+  if (y > 1960) {ball.style.left = "1px";}
+  if (y > 2160)  {ball.style.left = "320px";}
+  if(y > 2340) {ball.style.left = "1px";}
+  if (y > 2560)  {ball.style.left = "320px";}
     //else{ball.style.right = y + "-px";}
   //if (y > 40) {ball.style.webkitTransform = 'top:0px; bottom:30px;'}
   //else{ball.style.webkitTransform = '39px';}
   
-
-
+  //for ball rolling off of rotating "animations" & pinball
+  if (y > 1240) {ball.style.left = "250px";}
+  if (y > 1280) {ball.style.left = "200px";}
+  if (y > 1320) {ball.style.left = "150px";}
+  if (y > 1360) {ball.style.left = "100px";}
+  if (y > 1500) {ball.style.left = "150px";}
+  if (y > 1600) {ball.style.left = "200px";}
+  if (y > 1640) {ball.style.left = "250px";}
+  if (y > 1740) {ball.style.left = "200px";}
+  if (y > 1780) {ball.style.left = "150px";}
+  if (y > 1880) {ball.style.left = "100px";}
+  if (y > 1940) {ball.style.left = "150px";}
+  if (y > 1980) {ball.style.left = "200px";}
+  if (y > 2040) {ball.style.left = "250px";}
+  if (y > 2120) {ball.style.left = "200px";}
+  if (y > 2160) {ball.style.left = "150px";}
+  if (y > 2300) {ball.style.left = "100px";}
+ 
   if (y > 1180){p1_2.style.webkitTransform = 'rotate(-45deg)';}
     else {p1_2.style.webkitTransform = 'rotate(0deg)'}
+  //end of rotate animations and pinball
 
 
 //animation for ANIMATION header
