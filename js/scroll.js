4e92a97b714d0baa98e220624e462f40935119e9
jchurch2112 <32181630+jchurch2112@users.noreply.github.com>
2017-11-30 17:48:45 -0800

    more changes with pumpers and z index

--- js/scroll.js
+++ js/scroll.js
@@ -28,6 +28,7 @@ window.onscroll = function(){
   if (y > 2160)  {ball.style.left = "320px";}
   if(y > 2340) {ball.style.left = "1px";}
   if (y > 2560)  {ball.style.left = "320px";}
+  //if (y > 695) {ball.style.top = 15 + y * 1.5 + "px";}
     //else{ball.style.right = y + "-px";}
   //if (y > 40) {ball.style.webkitTransform = 'top:0px; bottom:30px;'}
   //else{ball.style.webkitTransform = '39px';}
@@ -37,7 +38,9 @@ window.onscroll = function(){
   if (y > 1280) {ball.style.left = "200px";}
   if (y > 1320) {ball.style.left = "150px";}
   if (y > 1360) {ball.style.left = "100px";}
+  if (y > 1400) {ball.style.left = "90px";}
   if (y > 1500) {ball.style.left = "150px";}
+  if (y > 1585) {ball.style.left = "180px";}
   if (y > 1600) {ball.style.left = "200px";}
   if (y > 1640) {ball.style.left = "250px";}
   if (y > 1740) {ball.style.left = "200px";}
@@ -47,15 +50,27 @@ window.onscroll = function(){
   if (y > 1980) {ball.style.left = "200px";}
   if (y > 2040) {ball.style.left = "250px";}
   if (y > 2120) {ball.style.left = "200px";}
-  if (y > 2160) {ball.style.left = "150px";}
+  if (y > 2160) {ball.style.left = "180px";}
+  if (y > 2220) {ball.style.left = "150px";}
+  if (y > 2280) {ball.style.left = "120px";}
+  if (y > 3359) {ball.style.left = "170px";}
   if (y > 2300) {ball.style.left = "100px";}
-  if (y > 2460) {ball.style.left = "150px";}
+  if (y > 2441) {ball.style.left = "150px";}
   if (y > 2472) {ball.style.left = "200px";}
   if (y > 2561) {ball.style.left = "250px";}
 
   if (y > 990){p1_2.style.webkitTransform = 'rotate(-45deg)';}
-    else {p1_2.style.webkitTransform = 'rotate(0deg)'}
+    else {p1_2.style.webkitTransform = 'rotate(0deg)';}
   //end of rotate animations and pinball
+  //this is the part that goes in the tube
+  if (y > 2610) {ball.style.left = "200px";}
+  if (y > 2674) {ball.style.left = "150px";}
+  if (y > 2722) {ball.style.left = "100px";}
+  if (y > 2802) {ball.style.left = "80px";}
+  //end of ball in tube
+  //tube moves down
+  if (y > 2805) {tube.style.top = 12 + y + px;}
+  //end of tube movement
 
 
 //animation for ANIMATION header
@@ -72,7 +87,32 @@ if (y > 1000) {header2.style.top = -12 + y * 1.0 + "px";}
   if (y > 1000) {header2.style.color = "skyblue";}
   else {header2.style.color = "black";}
 
-  //for bumper id's
+  //for bumper spring transitions
+if (y > 1481) {bumper1.style.webkitTransform = 'scale(1.5,1.5)';}
+  else {bumper1.style.webkitTransform = 'scale(1,1)';}
+  if (y > 1545){bumper1.style.webkitTransform = 'scale(1,1)';}
+
+if (y > 1713) {bumper2.style.webkitTransform = 'scale(1.5,1.5)';}
+  else {bumper2.style.webkitTransform = 'scale(1,1)';}
+  if (y > 1769){bumper2.style.webkitTransform = 'scale(1,1)';}
+
+  if (y > 1913) {bumper3.style.webkitTransform = 'scale(1.5,1.5)';}
+    else {bumper3.style.webkitTransform = 'scale(1,1)';}
+    if (y > 1953){bumper3.style.webkitTransform = 'scale(1,1)';}
+
+    if (y > 2114) {bumper4.style.webkitTransform = 'scale(1.5,1.5)';}
+      else {bumper4.style.webkitTransform = 'scale(1,1)';}
+      if (y > 2138){bumper4.style.webkitTransform = 'scale(1,1)';}
+
+      if (y > 2338) {bumper5.style.webkitTransform = 'scale(1.5,1.5)';}
+        else {bumper5.style.webkitTransform = 'scale(1,1)';}
+        if (y > 2450){bumper5.style.webkitTransform = 'scale(1,1)';}
+
+        if (y > 2586) {bumper6.style.webkitTransform = 'scale(2,2)';}
+          else {bumper6.style.webkitTransform = 'scale(1,1)';}
+          if (y > 2674){bumper6.style.webkitTransform = 'scale(1,1)';}
+//end of bumper spring transitions
+
 
 
 
