41f2fbddf8e44d6e4d89e26d13b3f4775b74c090
jchurch2112 <32181630+jchurch2112@users.noreply.github.com>
2017-12-04 21:50:38 -0800

    major upload
    bouncing ball and ending still need work

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,7 @@
 var canvas = document.getElementById("canvas");
 var ball = document.getElementById("ball");
+function myFunction() {
+var y = document.getElementById("pin").loop;}
 
 window.onscroll = function(){
 
@@ -12,26 +14,29 @@ window.onscroll = function(){
   ball.style.top = 12 + y * 1.0 + "px";
    ball.innerHTML = y + "px" + y * 0.97 + 12 + "px";
    //ball bounce on "ANIMATION"
-  //if (y > 40) {ball.style.top = 12 + "px";}
-  //if (y > 80) {ball. style.top = 12 + y + 1.1 + "px";}
-  //if (y > 120) {ball.style.top = 12 + "px";}
-  //if (y > 160) {ball. style.top = 12 + y * 1.0 + "px";}
-  if (y < 320) {ball.style.left = y + "px";}
+  
+  //if (y < 320) {ball.style.left = y + "px";}
   if (y > 40) {ball.style.top = "24px";}
    if (y >100){ball.style.top = 12 + y * 1.0 + "px";}
+   if (y > 133) {ball.style.top = "120px";}
+   if (y >185){ball.style.top = 12 + y * 1.0 + "px";}
+   if (y >213){ball.style.top = "200px";}
+   if (y >305){ball.style.top = 12 + y * 1.0 + "px";}
+   
+   if (y > 0){ball.style.left = "30px";}
+   if (y > 80) {ball.style.left = "110px";}
+   if (y > 160) {ball.style.left = "190px";}
+   if (y > 300) {ball.style.left = "305px";}
 
   //ball bounce on "ANIMATION" --end
   if(y > 1360) {ball.style.left =  "1px";}
 
- if (y > 1700)  {ball.style.left = "320px";}
-  if (y > 1960) {ball.style.left = "1px";}
-  if (y > 2160)  {ball.style.left = "320px";}
-  if(y > 2340) {ball.style.left = "1px";}
-  if (y > 2560)  {ball.style.left = "320px";}
-  //if (y > 695) {ball.style.top = 15 + y * 1.5 + "px";}
-    //else{ball.style.right = y + "-px";}
-  //if (y > 40) {ball.style.webkitTransform = 'top:0px; bottom:30px;'}
-  //else{ball.style.webkitTransform = '39px';}
+ //if (y > 1700)  {ball.style.left = "320px";}
+  //if (y > 1960) {ball.style.left = "1px";}
+  //if (y > 2160)  {ball.style.left = "320px";}
+  //if(y > 2340) {ball.style.left = "1px";}
+  //if (y > 2560)  {ball.style.left = "320px";}
+
 
   //for ball rolling off of rotating "animations" & pinball
   if (y > 1240) {ball.style.left = "250px";}
@@ -67,10 +72,23 @@ window.onscroll = function(){
   if (y > 2674) {ball.style.left = "150px";}
   if (y > 2722) {ball.style.left = "100px";}
   if (y > 2802) {ball.style.left = "80px";}
+            //words with tube
+               if (y > 2700) {p2.style.top = 780 + y / 1.3 + "px";} else {p2.style.top = "2820px";}
+            //end of words with tube
   //end of ball in tube
   //tube moves down
-  if (y > 2805) {tube.style.top = 12 + y + px;}
+  //if (y > 3100) {tube.style.bottom = y * 1.1 + "3100px";}
+  if (y > 2700) {tube.style.top = 550 + y / 1.3 + "px";} else { tube.style.top = "2600px";}
+  //if (y > 4900) {tube.style.top = 500 + y / 1.5 + "px";}
+  //if (y > 3110) {tube.style.top = 12 + y * 1.1 + "px";}
+   //else {tube.style.top = 
+
   //end of tube movement
+  //ball in tube
+  if (y > 2752) {ball.style.left = "150px";}
+  if (y > 4452) {ball.style.left = "100px";}
+
+  //end ball in tube
 
 
 //animation for ANIMATION header
@@ -113,7 +131,84 @@ if (y > 1713) {bumper2.style.webkitTransform = 'scale(1.5,1.5)';}
           if (y > 2674){bumper6.style.webkitTransform = 'scale(1,1)';}
 //end of bumper spring transitions
 
+//bumper sfx
+if (y > 1470) {pin1.play();}
+if (y > 1710) {pin1.pause();}
 
+if (y > 1710) {pin2.play();}
+if (y > 1916) {pin2.pause();}
+
+if (y > 1916) {pin3.play();}
+if (y > 2108) {pin3.pause();}
+
+if (y > 2108) {pin4.play();}
+if (y > 2329) {pin4.pause();}
+
+if (y > 2329) {pin5.play();}
+if (y > 2594) {pin5.pause();}
+
+if (y > 2594) {pin1.play();}
+if (y > 2816) {pin1.pause();}
+
+ 
+         
+
+//end bumper sfx
+//paper tearing
+if (y > 5980) {lp.style.webkitTransform = 'translateX(-30px)';} else {lp.style.webkitTransform = 'translate(0px)';}
+if (y > 5980) {rp.style.webkitTransform = 'translateX(25px)';} else {rp.style.webkitTransform = 'translate(0px)';}
+if (y > 6000) {lp.style.webkitTransform = 'translateX(-50px)';} else {lp.style.webkitTransform = 'translate(0px)';}
+if (y > 6000) {rp.style.webkitTransform = 'translateX(45px)';} else {lp.style.webkitTransform = 'translate(0px)';}
+//end of paper tearing
+
+//for 3 pages appearing
+if (y > 6000) {page1.style.webkitTransform = 'translateX(-30px)';}
+if (y > 6000) {page1.style.opacity = '100';}
+if (y > 6000) {page2.style.webkitTransform = 'translateX(-30px)';}
+if (y > 6000) {page2.style.opacity = '100';}
+if (y > 6000) {page3.style.webkitTransform = 'translateX(-30px)';}
+if (y > 6000) {page3.style.opacity = '100';}
+
+if (y > 6000) {p3.style.webkitTransform = 'translateY(-20px)';}
+if(y > 6000) {p3.style.color = 'black';}
+if(y > 6000) {p3.style.opacity = '100';}
+
+
+//end of 3 pages appearing
+
+//going down the slide
+if (y > 6700){ball.style.left = '150px';} 
+if (y > 6740){ball.style.left = '200px';}
+if (y > 6840){ball.style.left = '250px';}
+if (y > 6940){ball.style.left = '300px';}
+
+//end of going down the slide
+
+//dollar sign away
+if (y > 6999){dollar.style.webkitTransform = 'rotate(120deg)';} else {dollar.style.webkitTransform = 'rotate(0deg)';}
+if (y > 6999) {dollar.style.opacity = '0';} else {dollar.style.opacity = '100';}
+if (y > 6999){dollar.style.webkitTransform = 'translateX(150px)';}
+ 
+
+//end of dollar sign away
+//cpu turns on
+if (y > 7300){p5.style.opacity = '100';} else {p5.style.opacity = '0';}
+//end of cpu turns on
+
+//text from bumpers
+   if (y > 1454) {b1.style.webkitTransform = 'translateY(100px)';}
+   if (y > 1727) {b2.style.webkitTransform = 'translateY(100px)';}
+   if (y > 1909) {b3.style.webkitTransform = 'translateY(100px)';}
+   if (y > 2090) {b4.style.webkitTransform = 'translateY(100px)';}
+   if (y > 2300) {b5.style.webkitTransform = 'translateY(100px)';}
+   
+   //this half is for opacity
+   if (y > 1454) {b1.style.opacity = '100';}
+   if (y > 1727) {b2.style.opacity = '100';}
+   if (y > 1909) {b3.style.opacity = '100';}
+   if (y > 2090) {b4.style.opacity = '100';}
+   if (y > 2300) {b5.style.opacity = '100';}
+//end of text from bumpers
 
 
 
